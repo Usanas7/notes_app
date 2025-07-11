6e8ed0ffbb86ba41e065168c1205910994a4db90
usanas7 <c.usanase@alustudent.com>
2025-07-11 12:03:57 +0200

    initial commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
+
 allprojects {
     repositories {
         google()
